6979d1d0b38feaca139211ab03b2487e93ded38a
Jang_Desktop <sdsd090811@gmail.com>
2022-08-03 13:55:07 +0900

    Add 스프링 시큐리티 2장 내용

---
+++ Springboot 시큐리티/src/security1/src/main/java/com/example/security1/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.example.security1.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity // 스프링 시큐리티 필터가 스프링 필터체인에 등록이 된다.
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable();
+		http.authorizeHttpRequests() // 어떤 request가 들어온다면.
+			.antMatchers("/user/**").authenticated() // /user/** 이런주소로 들어오면 인증이 필요해
+			.antMatchers("/manager/**").hasRole("ROLE_ADMIN or ROLE_MANAGER") // /manager/** 이런 주소로 들어오면 인증뿐만 아니라 "ROLE_ADMIN or ROLE_MANAGER" 권한이 있는 사람만 들어오게 할거야 
+			.antMatchers("/admin/**").hasRole("ROLE_ADMIN")
+			.anyRequest().permitAll() // 다른 모든 request는 권한 허용.
+			/* 아래코드들은 권한없는 접근을 로그인페이지로 이동시키는 코드들*/
+			.and()
+			.formLogin()
+			.loginPage("/login");
+	}
+}
--- Springboot 시큐리티/src/security1/src/main/java/com/example/security1/controller/indexController.java
+++ Springboot 시큐리티/src/security1/src/main/java/com/example/security1/controller/indexController.java
@@ -2,13 +2,47 @@ package com.example.security1.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class indexController {
 
+	//로그인한 사람만 접근
 	@GetMapping({"", "/"})
 	public String index() {
 		return "index";
 	}
 	
+	//로그인한 사람만 접근
+	@GetMapping("/user")
+	public @ResponseBody String user() {
+		return "user";
+	}
+	
+	//로그인한 사람중에 admin 권한이 있는 사람만
+	@GetMapping("/admin")
+	public @ResponseBody String admin() {
+		return "admin";
+	}
+	
+	//로그인한 사람중에 manager 권한이 있는 사람만 
+	@GetMapping("/manager")
+	public @ResponseBody String manager() {
+		return "manager";
+	}
+	
+	@GetMapping("/login")
+	public @ResponseBody String login() {
+		return "login";
+	}
+	
+	@GetMapping("/join")
+	public @ResponseBody String join() {
+		return "join";
+	}
+	
+	@GetMapping("/joinProc")
+	public @ResponseBody String joinProc() {
+		return "회원가입 완료됨";
+	}
 }
