bb90b5af24688ecfeb1242ae8c60f60767782c31
Jang_Desktop <sdsd090811@gmail.com>
2022-08-05 10:55:05 +0900

    Add 스프링 시큐리티 김기태교수님 강의 내용

--- Springboot 시큐리티/src/security1/src/main/java/com/example/security1/config/auth/simpleway/SecurityUser.java
+++ Springboot 시큐리티/src/security1/src/main/java/com/example/security1/config/auth/simpleway/SecurityUser.java
@@ -6,9 +6,11 @@ import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.User;
 
 public class SecurityUser extends User{
-
+	private com.example.security1.model.User user;
+	
 	public SecurityUser(com.example.security1.model.User user) {
 		super(user.getUsername(), user.getPassword(), AuthorityUtils.createAuthorityList(user.getRole()));
+		this.user = user;
 	}
 	
 }
